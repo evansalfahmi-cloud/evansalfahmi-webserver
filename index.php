7dea3d63ba4c02785cb99c522dac14245f8ce22c
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2024-12-11 10:48:34 +0000

    edit deskripsi card text

--- index.php
+++ index.php
@@ -65,8 +65,8 @@
                 <div class="card">
                     <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">SMARTPHONE</h5>
+                        <p class="card-text">"Temukan beragam tools dan software terkini, serta tips dan trik pintar untuk mengoptimalkan pengalaman smartphone Anda.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
@@ -75,8 +75,8 @@
                 <div class="card">
                     <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title 2</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">COMPUTER</h5>
+                        <p class="card-text">Jelajahi tools eksklusif, software terbaik, dan trik praktis untuk memaksimalkan perangkat Apple Anda sehari-hari.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
@@ -85,7 +85,7 @@
                 <div class="card">
                     <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title 3</h5>
+                        <h5 class="card-title">APPLE DEVICE</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
