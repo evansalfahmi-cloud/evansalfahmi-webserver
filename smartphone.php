7a0b8b55b13466cfffebf702a0fbd15e6a212238
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2024-12-15 01:11:10 +0000

    replace css and js

--- smartphone.php
+++ smartphone.php
@@ -1,5 +1,19 @@
-<!-- about.php -->
 <?php include 'includes/header.php'; ?>
-<h1>SMARTPHONE</h1>
-<p>This is the firmware page. Here you can put information about your website or business.</p>
+<h1 class="mt-4">Tools untuk Smartphone</h1>
+<p class="text-muted">Beragam tools dan aplikasi untuk meningkatkan produktivitas smartphone Anda.</p>
+
+<div class="row">
+    <!-- Contoh tools -->
+    <div class="col-md-4">
+        <div class="card">
+            <img src="img/smartphone.png" class="card-img-top" alt="Smartphone Tool">
+            <div class="card-body">
+                <h5 class="card-title">Tool A</h5>
+                <p class="card-text">Deskripsi singkat tentang Tool A untuk smartphone.</p>
+                <a href="#" class="btn btn-primary">Download</a>
+            </div>
+        </div>
+    </div>
+    <!-- Tambahkan tools lainnya -->
+</div>
 <?php include 'includes/footer.php'; ?>
