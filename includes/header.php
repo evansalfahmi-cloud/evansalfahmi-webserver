8b3f67cd896d6b4d862e41634f71164ae43fff3e
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2024-12-15 11:09:39 +0000

    meambahkan icon brand  pada kategori brand pada firmware_index.php

--- includes/header.php
+++ includes/header.php
@@ -8,12 +8,14 @@
     <!-- Link ke file CSS Bootstrap -->
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="icon" href="../img/ico.png" type="image/x-icon"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container">
-            <a class="navbar-brand" href="../index.php#"><h1 class="h3 m-0">evansalfahmi-web</h1></a>
+            <a class="navbar-brand" href="../index.php#">
+            <h1 class="h3 m-0">evansalfahmi-web</h1></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- smartphone/firmware_index.php
+++ smartphone/firmware_index.php
@@ -4,54 +4,64 @@ include '../includes/header.php';
 include '../includes/config.php';
 ?>
 
-<div class="row">
+<div class="row g-4">
     <div class="col-md-4">
         <div class="card">
-            <img src="img/vivo.png" class="card-img-top" alt="Vivo">
+            <img src="../img/vivo.png" class="card-img-top" alt="Vivo">
             <div class="card-body">
                 <h5 class="card-title">Firmware Vivo</h5>
-                <p class="card-text">Dapatkan firmware terbaru untuk perangkat Vivo.</p>
+                <p class="card-text">Dapatkan firmware terbaru untuk Vivo.</p>
                 <a href="firmware.php?brand=Vivo" class="btn btn-primary">Lihat Firmware Vivo</a>
             </div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card">
-            <img src="img/oppo.png" class="card-img-top" alt="Oppo">
+            <img src="../img/oppo.png" class="card-img-top" alt="Oppo">
             <div class="card-body">
                 <h5 class="card-title">Firmware Oppo</h5>
-                <p class="card-text">Dapatkan firmware terbaru untuk perangkat Oppo.</p>
+                <p class="card-text">Dapatkan firmware terbaru untuk Oppo.</p>
                 <a href="firmware.php?brand=oppo" class="btn btn-primary">Lihat Firmware Oppo</a>
             </div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card">
-            <img src="img/samsung.png" class="card-img-top" alt="Samsung">
+            <img src="../img/samsung.png" class="card-img-top" alt="Samsung">
             <div class="card-body">
                 <h5 class="card-title">Firmware Samsung</h5>
-                <p class="card-text">Dapatkan firmware terbaru untuk perangkat Samsung.</p>
+                <p class="card-text">Dapatkan firmware terbaru untuk Samsung.</p>
                 <a href="firmware.php?brand=Samsung" class="btn btn-primary">Lihat Firmware Samsung</a>
             </div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card">
-            <img src="img/oppo.png" class="card-img-top" alt="Infinix">
+            <img src="../img/infinix.png" class="card-img-top" alt="Infinix">
             <div class="card-body">
                 <h5 class="card-title">Firmware Infinix</h5>
-                <p class="card-text">Dapatkan firmware terbaru untuk perangkat Infinix.</p>
+                <p class="card-text">Dapatkan firmware terbaru untuk Infinix.</p>
                 <a href="firmware.php?brand=Infinix" class="btn btn-primary">Lihat Firmware Infinix</a>
             </div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card">
-            <img src="img/samsung.png" class="card-img-top" alt="Realme">
+            <img src="../img/realme.png" class="card-img-top" alt="Realme">
             <div class="card-body">
                 <h5 class="card-title">Firmware Realme</h5>
-                <p class="card-text">Dapatkan firmware terbaru untuk perangkat Realme.</p>
+                <p class="card-text">Dapatkan firmware terbaru untuk Realme.</p>
                 <a href="firmware.php?brand=Realme" class="btn btn-primary">Lihat Firmware Realme</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card">
+            <img src="../img/xiaomi.png" class="card-img-top" alt="Xiaomi">
+            <div class="card-body">
+                <h5 class="card-title">Firmware Xiaomi</h5>
+                <p class="card-text">Dapatkan firmware terbaru untuk Xiaomi.</p>
+                <a href="firmware.php?brand=Xiaomi" class="btn btn-primary">Lihat Firmware Xiaomi</a>
             </div>
         </div>
     </div>
