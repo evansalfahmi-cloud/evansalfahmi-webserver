187454e7c687aeccea2a0c45c6e0456f142447e5
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2024-12-15 08:27:29 +0000

    edit halaman software flash menjadi tabel

--- includes/header.php
+++ includes/header.php
@@ -23,16 +23,16 @@
                         <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="tools.php">News & Tips</a>
+                        <a class="nav-link" href="../tools.php">News & Tips</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="operating_system.php">Shop</a>
+                        <a class="nav-link" href="../operating_system.php">Shop</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
+                        <a class="nav-link" href="../about.php">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
+                        <a class="nav-link" href="../contact.php">Contact</a>
                     </li>
                 </ul>
             </div>
--- smartphone/software_flash.php
+++ smartphone/software_flash.php
@@ -3,31 +3,38 @@
 include '../includes/config.php';
 
 // Query untuk mengambil data software flash
-$query = "SELECT * FROM software_flash";
-$result = mysqli_query($conn, $query);
-$query = "SELECT * FROM software_flash";
+
+$query = "SELECT * FROM software_flash ORDER BY name ASC";
 $result = mysqli_query($conn, $query);
 ?>
 
 <div class="container mt-5">
-        <h2>Software Flash HP</h2>
-        <div class="row">
-            <?php
-            // Tampilkan data software flash
-            while ($row = mysqli_fetch_assoc($result)): ?>
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo htmlspecialchars($row['name']); ?></h5>
-                            <p class="card-text"><?php echo htmlspecialchars($row['description']); ?></p>
-                            <a href="<?php echo htmlspecialchars($row['download_link']); ?>" class="btn btn-primary">Download</a>
-                        </div>
-                    </div>
-                </div>
-            <?php endwhile; ?>
-        </div>
+        <h2 class="text-center">Software Flash List</h2>
+        <table class="table table-striped table-hover mt-4">
+            <thead class="table-primary">
+                <tr>
+                    <th>#</th>
+                    <th>Nama Software</th>
+                    <th>Deskripsi</th>
+                    <th>Link Download</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $no = 1;
+                while ($row = mysqli_fetch_assoc($result)): ?>
+                    <tr>
+                        <td><?php echo $no++; ?></td>
+                        <td><?php echo htmlspecialchars($row['name']); ?></td>
+                        <td><?php echo htmlspecialchars($row['description']); ?></td>
+                        <td>
+                            <a href="<?php echo htmlspecialchars($row['download_link']); ?>" class="btn btn-primary btn-sm" target="_blank">Download</a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
     </div>
-
 <?php include '../includes/footer.php'; ?>
 <?php
 // Tutup koneksi
